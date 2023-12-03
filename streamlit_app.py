62fbf5beb8015077b57da3cb208248c1ff978e6b
skhushwanttraining <144827245+skhushwanttraining@users.noreply.github.com>
2023-12-03 16:07:00 -0700

    Update streamlit_app.py
    Created another header for a special menu item

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,4 @@ streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range eggs')
 streamlit.text('🥑🍞 Avocado Toast')
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
